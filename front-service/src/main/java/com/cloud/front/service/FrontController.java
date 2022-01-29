febdcd8b124877ae2c798d8c2187044b958649e9
sargassov <sargassov@yandex.ru>
2022-01-29 16:34:30 +0300

    Initial commit

---
+++ front-service/src/main/java/com/cloud/front/service/FrontController.java
@@ -0,0 +1,38 @@
+package com.cloud.front.service;
+
+import com.cloud.common.ProductDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@EnableEurekaClient
+@RequiredArgsConstructor
+//@CrossOrigin("*") // Homework: *
+public class FrontController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+//    private static final Function<Product, ProductDto> mapper = p -> new ProductDto(p.getId(), p.getTitle(), p.getPrice());
+
+    @GetMapping
+    public List<ProductDto> findAll() {
+        return restTemplate.getForObject("http://product-serice/api/v1/products'", List.class);
+    }
+
+}
